422ce953eedf6ec94a69feb4b4f32a0f86e1674f
Macrow <370008622@qq.com>
2023-06-11 22:39:55 +0800

    增加了数据库

--- sql.py
+++ sql.py
@@ -15,18 +15,74 @@ def create_charging_stations_table():
          station_type TEXT,
          status TEXT,
          current_charging_car TEXT,
-         charging_queue TEXT)
+         charging_queue TEXT,
+         chrrent_waiting_car TEXT)
+    ''')
+
+        #事件表
+    c.execute('''
+        CREATE TABLE event_list
+        (event_id TEXT PRIMARY KEY,
+         event_type TEXT,
+         charge_type TEXT,
+         event_value TEXT,
+         event_time TEXT
+         )
+    ''')
+
+    #队列表
+    c.execute('''
+        CREATE TABLE wait_queue
+        (queue_id TEXT PRIMARY KEY,
+         queue_value TEXT,
+         queue_type TEXT,
+         queue_order TEXT
+         )
+    ''')
+
+    #详单表
+    c.execute('''
+        CREATE TABLE detail_bill
+        ( detail_id TEXT PRIMARY KEY,
+         car_id TEXT,
+         detail_time TEXT,
+         charge_id TEXT,
+         charge_sum TEXT,
+         chatge_time TEXT,
+         start_time TEXT,
+         end_time TEXT,
+         charge_fee TEXT,
+         server_fee TEXT,
+         total_fee TEXT
+         )
+    ''')
+
+     #账单表
+    c.execute('''
+        CREATE TABLE bill
+        (car_id TEXT,
+         bill_id TEXT PRIMARY KEY,
+         bill_time TEXT,
+         charge_sum TEXT,
+         chatge_time TEXT,
+         start_time TEXT,
+         end_time TEXT,
+         charge_fee TEXT,
+         server_fee TEXT,
+         total_fee TEXT,
+         detail_list TEXT
+         )
     ''')
 
     # 初始插入充电桩数据，其中 station_id 是唯一的，分别为 A, B, C, D, E
-    stations = [("A", "fast", "free", "", ""),
-                ("B", "fast", "free", "", ""),
-                ("C", "slow", "free", "", ""),
-                ("D", "slow", "free", "", ""),
-                ("E", "slow", "free", "", "")]
+    stations = [("A", "fast", "free", "", "", ""),
+                ("B", "fast", "free", "", "", ""),
+                ("C", "slow", "free", "", "", ""),
+                ("D", "slow", "free", "", "", ""),
+                ("E", "slow", "free", "", "", "")]
 
     c.executemany('''
-        INSERT INTO charging_stations VALUES (?,?,?,?,?)
+        INSERT INTO charging_stations VALUES (?,?,?,?,?,?)
     ''', stations)
 
     # 提交事务
@@ -34,15 +90,7 @@ def create_charging_stations_table():
 
     # 关闭到数据库的连接
     conn.close()
-# def requircs():
-#     conn = sqlite3.connect('charging_stations.db')
-#     #
-#     #     # 创建一个Cursor对象
-#     c = conn.cursor()
-#     c.execute("SELECT * FROM charging_stations")
-#     stations = c.fetchall()
-#     for station in stations:
-#         print(station)
+
 if __name__ == "__main__":
     create_charging_stations_table()
     # requircs()
